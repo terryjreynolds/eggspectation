73f5f0b7e9633a2ee8afbefe3a599c157a9d8abb
Terry Reynolds <terrancejreynolds@gmail.com>
2018-12-24 22:30:50 -0500

    added more blurred photos

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function showHDImages() {
-  console.log("im in");
+  console.log("im in 1");
 
   let bigBurger = document.getElementById("bigBurger1");
   let smallBurger = document.getElementById("smallBurger1");
   smallBurger.className = "burgerHidden";
   bigBurger.className = "burgerFadeInOnLoad";
+}
+function showHDEggstack() {
+  console.log("im in 2");
+
+  let eggstack = document.getElementById("eggstack1");
+  let smallEggstack = document.getElementById("smallEggstack1");
+  smallEggstack.className = "burgerHidden";
+  eggstack.className = "burgerFadeInOnLoad";
+}
+function showHDPancakes() {
+  console.log("im in 3");
+
+  let pancakes = document.getElementById("pancakes1");
+  let smallPancakes = document.getElementById("smallPancakes1");
+  smallPancakes.className = "burgerHidden";
+  pancakes.className = "burgerFadeInOnLoad";
 }
